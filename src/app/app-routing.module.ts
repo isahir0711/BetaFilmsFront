e93edc146e332ddaed40871c314c49beec90c874
Isahir Zavala Miranda <isahirzavala@hotmail.com>
2023-09-29 00:46:26 -0500

    plans page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './landing/home/home.component';
+import { PlansComponent } from './landing/plans/plans.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
+  {path:"Plans",component:PlansComponent},
   
   //wildcard route
   {path: '**', redirectTo: ''}
